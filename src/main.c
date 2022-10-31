b9b9e14d8f05fad6a9cf2b802023be590eecdf9f
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2022-10-31 22:13:46 +0700

    change input from stdin

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include "./ADT/mesinkata/mesinkata.h"
 #include "boolean.h"
 
 #define MAX_LENGTH 100
 
-boolean isEqual(char *str1, char *str2)
+boolean isEqual(Word str1, char *str2)
 {
     int i = 0;
     boolean isDiffer = false;
-    while (str1[i] != '\0' && str2[i] != '\0' && !isDiffer)
+    while (str1.TabWord[i] != '\0' && str2[i] != '\0' && !isDiffer)
     {
-        printf("%c %c\n", str1[i], str2[i]);
-        if (str1[i] != str2[i])
+        if (str1.TabWord[i] != str2[i])
             isDiffer = true;
         i++;
     }
@@ -19,47 +19,45 @@ boolean isEqual(char *str1, char *str2)
 
 int main()
 {
-    char inp[MAX_LENGTH];
     printf("ENTER COMMAND: ");
-    scanf("%s", inp);
-    if (isEqual(inp, "START"))
+    STARTWORD(false);
+    if (isEqual(currentWord, "START"))
     {
         // START()
         printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.");
     }
-    else if (isEqual(inp, "LOAD"))
+    else if (isEqual(currentWord, "LOAD"))
     {
     }
-    else if (isEqual(inp, "SAVE"))
+    else if (isEqual(currentWord, "SAVE"))
     {
     }
-    else if (isEqual(inp, "CREATEGAME"))
+    else if (isEqual(currentWord, "CREATEGAME"))
     {
-        char gameName[MAX_LENGTH];
         printf("Masukkan nama game yang akan ditambahkan: ");
-        scanf("%s\n", gameName);
+        ADVWORD();
         // Perlu checker??
-        if (isEqual(gameName, "RNG") || isEqual(gameName, "Diner Dash"))
+        if (isEqual(currentWord, "RNG") || isEqual(currentWord, "Diner Dash"))
             printf("Game sudah ada");
         else
             printf("Game berhasil ditambahkan");
     }
-    else if (isEqual(inp, "LISTGAME"))
+    else if (isEqual(currentWord, "LISTGAME"))
     {
     }
-    else if (isEqual(inp, "DELETEGAME"))
+    else if (isEqual(currentWord, "DELETEGAME"))
     {
     }
-    else if (isEqual(inp, "QUEUEGAME"))
+    else if (isEqual(currentWord, "QUEUEGAME"))
     {
     }
-    else if (isEqual(inp, "PLAYGAME"))
+    else if (isEqual(currentWord, "PLAYGAME"))
     {
     }
-    else if (isEqual(inp, "QUIT"))
+    else if (isEqual(currentWord, "QUIT"))
     {
     }
-    else if (isEqual(inp, "HELP"))
+    else if (isEqual(currentWord, "HELP"))
     {
     }
     // Command lain
