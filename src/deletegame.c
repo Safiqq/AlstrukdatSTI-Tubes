ad5910fa8c577d19d9a784e20218858e6f658fb6
oncarrozaqy <oncarrozaqyy@gmail.com>
2022-11-03 19:42:59 +0700

    Upload mentahan playgame dan deletegame

---
+++ src/deletegame.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "./ADT/queue/queue.h"
+#include "./ADT/mesinkata/mesinkata.h"
+#include "boolean.h"
+
+void deletegame(Queue *listgame, Queue antriangame){
+    printf("Berikut adalah daftar game yang tersedia\n");
+    for(int i = 0; i < length(*listgame); i++){
+        printf("\t%d.%s\n", i, (*listgame).buffer[i]);
+    }
+    int hapus;
+    if (hapus<=5 && hapus>=1){
+        printf("Game gagal dihapus.\n");
+    }
+    else{
+        ElType game;
+        game=(*listgame).buffer[hapus-1];
+        boolean isInAntrian= false;
+        int i=0;
+        while ((i<length(antriangame)) && !isInAntrian) {
+            if (game==antriangame.buffer[i]){
+                isInAntrian=true;
+            }
+            i++;
+        }
+        if (isInAntrian){
+            printf("Game gagal dihapus.\n");
+        }
+        else{
+            for (int i=hapus-1; i<length(*listgame);i++){
+                if (i!=length(*listgame)-1){
+                    (*listgame).buffer[i]=(*listgame).buffer[i+1];
+                }
+                else{
+                    ElType val;
+                    dequeue(listgame,&val);
+                }
+            }
+            printf("Game berhasil dihapus.\n");
+        }
+    }
+}
---
+++ src/playgame.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "./ADT/queue/queue.h"
+#include "./ADT/mesinkata/mesinkata.h"
+
+void playgame(Queue *antriangame){
+    ElType play;
+    printf("Berikut adalah daftar game milikmu: \n");
+    for(int i =0;i<length(*antriangame);i++){
+        printf("%d. %c",i,antriangame->buffer[i]);
+    }
+    if (!isEmpty(*antriangame)){
+        ElType val;
+        dequeue(antriangame, &val);
+        play=val;
+    }
+    if (play==1){
+        //dinnerdash();
+    }
+    else if (play==2){
+        //rng();
+    }
+    else{
+        printf("Game %s masih dalam maintenance, belum dapat dimainkan. SIlakan pilih game lain.",play);
+    }
+}
