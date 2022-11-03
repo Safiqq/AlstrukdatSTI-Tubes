8a7aa17938ea7d508f10c60880760bc455bfd0f4
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2022-11-03 21:51:57 +0700

    update start

--- src/start.c
+++ src/start.c
@@ -2,23 +2,21 @@
 #include <stdlib.h>
 #include "boolean.h"
 #include "./ADT/mesinkata/mesinkata.h"
-#include "./ADT/queue/queue.h"
+#include "./ADT/array/array.h"
 
 #define MAX_LENGTH 100
 
-void BNMO_START(Queue *Q)
+void BNMO_START(TabStr *T)
 {
     int i, j, len;
-    CreateQueue(Q);
+    MakeEmpty(T);
     isFile = true;
-    STARTWORD();
+    STARTWORD("../data/config.txt", "r");
     len = atoi(currentWord.TabWord);
     for (i = 0; i < len; i++)
     {
         ADVLINE();
-        printf("%s\n", currentWord.TabWord);
-
-        // ElType = char* = string
-        enqueue(Q, currentWord.TabWord);
+        // Bug, all elements [0..len-2] replaced by last el
+        SetEl(T, i, currentWord.TabWord);
     }
 }
