cddd436061550438bb6c58618777e735127e2642
DamianMarvel7 <110451781+DamianMarvel7@users.noreply.github.com>
2022-11-05 06:50:38 +0700

    save baru

--- src/save.c
+++ src/save.c
@@ -1,34 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "./ADT/array/array.h"
-#include "./ADT/mesinkata/mesinkata.h"
 
-char *concat(char a[500],char b[500])
-{
-  int i=0,j=0;
-  while(a[i]!='\0')
-  i++;
-  while(b[j]!='\0')
-  {
-    a[i]=b[j];
-    j++;
-    i++;
-  }
-  a[i]='\0';
-  return a;
-}
-
-void save(char namafile[100], TabStr arraygame){
-    FILE*pita;
-    char path[10] = "../data/";
-    pita=fopen(concat(path,namafile),"w");
-
-    fprintf(pita,"%c\n",(char)(NbElmt(arraygame)+48));
-
-    for(int i=0;i<NbElmt(arraygame);i++){
-        fprintf(pita,"%s\n",arraygame.TI[i]);
+void save(char * namafile, TabStr listgame){
+    FILE*txt;
+    char path[100] = "../data/";
+    int i = 8;
+    while(*namafile!='\0'){
+      path[i] = *namafile;
+      i++;
+      *namafile++;
     }
-    fclose(pita);
+    txt=fopen(path,"w");
+
+    fprintf(txt,"%c\n",(char)(listgame.Neff+48));
+
+    for(int i=0;i<NbElmt(listgame)-1;i++){
+        fprintf(txt,"%s\n",listgame.TI[i]);
+    }
+    fprintf(txt,"%s",listgame.TI[NbElmt(listgame)-1]);
+
+    fclose(txt);
     printf("Save file berhasil disimpan.\n");
 }
-
