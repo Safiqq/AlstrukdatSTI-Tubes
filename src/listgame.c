ac7aa9e5ac3fe7f0142f6dc21ae44e7e40c51cc1
oncarrozaqy <oncarrozaqyy@gmail.com>
2022-11-07 21:58:18 +0700

    update penomoran listgame

--- src/listgame.c
+++ src/listgame.c
@@ -8,6 +8,6 @@ void listgame(TabStr T)
     printf("Berikut adalah daftar game yang tersedia\n");
     for (i = 0; i < T.Neff; i++)
     {
-        printf(" %d.%s\n", i, T.TI[i]);
+        printf(" %d.%s\n", i+1, T.TI[i]);
     }
 }
