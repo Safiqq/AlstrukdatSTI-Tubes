5d7677a8953f883f1c4d98de8d8dd2de60a172ae
MUHAMAD PEDRO <muhamadpdr03@gmail.com>
2022-11-11 13:14:46 +0700

    buat test.c dan update fungsi primitifnya

--- src/ADT/arrayTTT/array.c
+++ src/ADT/arrayTTT/array.c
@@ -10,21 +10,6 @@ int LengthArr(TabChr T)
     return T.Neff;
 }
 
-ElTypeArr GetArr(TabChr T, IdxType i)
-{
-    return T.TI[i];
-}
-
-void CopyArr(TabChr Tin, TabChr *Tout)
-{
-    int i;
-    for (i = 0; i < LengthArr(Tin); i++)
-    {
-        Tout->TI[i] = Tin.TI[i];
-    }
-    Tout->Neff = Tin.Neff;
-}
-
 void SetArr(TabChr *T, IdxType i, ElTypeArr v)
 {
     T->TI[i] = v;
@@ -40,24 +25,4 @@ void SetNeffArr(TabChr *T, IdxType N)
 boolean IsEmptyArr(TabChr T)
 {
     return T.Neff == 0;
-}
-
-boolean IsFullArr(TabChr T)
-{
-    return T.Neff == CAPACITY;
-}
-
-void PrintArr(TabChr T)
-{
-    if (IsEmptyArr(T))
-        printf("Tabel kosong\n");
-    else
-    {
-        int i;
-        for (i = 0; i < LengthArr(T); i++)
-        {
-            // printf("%d\n", i);
-            printf("%c\n", T.TI[i]);
-        }
-    }
 }
---
+++ src/ADT/arrayTTT/test.c
@@ -0,0 +1,27 @@
+#include "array.h"
+
+int main(){
+    // Inisialisasi T
+    TabChr T;
+
+    // Check fungsi CreateArr
+    CreateArr(&T);
+
+    // Check isEmptyArr sekaligus check apakah T berhasil dibuat
+    printf("isEmptyArr -> Jika T berhasil dibuat maka output 1 atau jika gagal output 0 : %d\n", IsEmptyArr(T));
+
+    // Check apakah sudah berhasil CreateArr sekaligus check lengthnya
+    printf("T berhasil dibuat dengan Neffnya : %d\n", LengthArr(T));
+
+    // Check fungsi SetArr
+    SetArr(&T, 0, 1);
+    printf("Array baru saja ditambahkan sehingga Neffnya menjadi : %d\n", LengthArr(T));
+    printf("Array yang ditambahkan yaitu : %d dengan index ke-%d\n", T.TI[0], 0);
+    SetArr(&T, 5, 0);
+    printf("Array baru saja ditambahkan sehingga Neffnya menjadi : %d\n", LengthArr(T));
+    printf("Array yang ditambahkan yaitu : %d dengan index ke-%d\n", T.TI[5], 5);
+
+
+
+    return 0;
+}
