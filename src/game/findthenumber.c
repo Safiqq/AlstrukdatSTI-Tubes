cb349711da32f024775abb8af6829511e94cacd1
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2022-12-02 20:58:08 +0700

    add skor

--- src/game/findthenumber.c
+++ src/game/findthenumber.c
@@ -4,12 +4,12 @@ void findTheNumber(ArrayMap *arrSB)
 {
     int x = (rand() % 32) + 31;
     BinTree T = NewBT(0,
-                        NewBT(1,
-                                NewBT(3, NewBT(7, NewBT(15, NewBT(31, NULL, NULL), NewBT(32, NULL, NULL)), NewBT(16, NewBT(33, NULL, NULL), NewBT(34, NULL, NULL))), NewBT(8, NewBT(17, NewBT(35, NULL, NULL), NewBT(36, NULL, NULL)), NewBT(18, NewBT(37, NULL, NULL), NewBT(38, NULL, NULL)))),
-                                NewBT(4, NewBT(9, NewBT(19, NewBT(39, NULL, NULL), NewBT(40, NULL, NULL)), NewBT(20, NewBT(41, NULL, NULL), NewBT(42, NULL, NULL))), NewBT(10, NewBT(21, NewBT(43, NULL, NULL), NewBT(44, NULL, NULL)), NewBT(22, NewBT(45, NULL, NULL), NewBT(46, NULL, NULL))))),
-                        NewBT(2,
-                                NewBT(5, NewBT(11, NewBT(23, NewBT(47, NULL, NULL), NewBT(48, NULL, NULL)), NewBT(24, NewBT(49, NULL, NULL), NewBT(50, NULL, NULL))), NewBT(12, NewBT(25, NewBT(51, NULL, NULL), NewBT(52, NULL, NULL)), NewBT(26, NewBT(53, NULL, NULL), NewBT(54, NULL, NULL)))),
-                                NewBT(6, NewBT(13, NewBT(27, NewBT(55, NULL, NULL), NewBT(56, NULL, NULL)), NewBT(28, NewBT(57, NULL, NULL), NewBT(58, NULL, NULL))), NewBT(14, NewBT(29, NewBT(59, NULL, NULL), NewBT(60, NULL, NULL)), NewBT(30, NewBT(61, NULL, NULL), NewBT(62, NULL, NULL))))));
+                    NewBT(1,
+                        NewBT(3, NewBT(7, NewBT(15, NewBT(31, NULL, NULL), NewBT(32, NULL, NULL)), NewBT(16, NewBT(33, NULL, NULL), NewBT(34, NULL, NULL))), NewBT(8, NewBT(17, NewBT(35, NULL, NULL), NewBT(36, NULL, NULL)), NewBT(18, NewBT(37, NULL, NULL), NewBT(38, NULL, NULL)))),
+                        NewBT(4, NewBT(9, NewBT(19, NewBT(39, NULL, NULL), NewBT(40, NULL, NULL)), NewBT(20, NewBT(41, NULL, NULL), NewBT(42, NULL, NULL))), NewBT(10, NewBT(21, NewBT(43, NULL, NULL), NewBT(44, NULL, NULL)), NewBT(22, NewBT(45, NULL, NULL), NewBT(46, NULL, NULL))))),
+                    NewBT(2,
+                        NewBT(5, NewBT(11, NewBT(23, NewBT(47, NULL, NULL), NewBT(48, NULL, NULL)), NewBT(24, NewBT(49, NULL, NULL), NewBT(50, NULL, NULL))), NewBT(12, NewBT(25, NewBT(51, NULL, NULL), NewBT(52, NULL, NULL)), NewBT(26, NewBT(53, NULL, NULL), NewBT(54, NULL, NULL)))),
+                        NewBT(6, NewBT(13, NewBT(27, NewBT(55, NULL, NULL), NewBT(56, NULL, NULL)), NewBT(28, NewBT(57, NULL, NULL), NewBT(58, NULL, NULL))), NewBT(14, NewBT(29, NewBT(59, NULL, NULL), NewBT(60, NULL, NULL)), NewBT(30, NewBT(61, NULL, NULL), NewBT(62, NULL, NULL))))));
     srand(time(NULL));
     printf("Gunakan full screen untuk experience yang lebih baik :)\nTekan ENTER untuk melanjutkan...\n");
     STARTWORD("", "");
@@ -35,6 +35,6 @@ void findTheNumber(ArrayMap *arrSB)
     else
         printf("Noob! Kamu gagal!\n");
     int score = ROOT(T) == x ? 100 : 0;
-    // printf("Skor: %d\n", score);
+    printf("Skor: %d\n", score);
     InsertSB(&arrSB->TIMap[7], score);
 }
